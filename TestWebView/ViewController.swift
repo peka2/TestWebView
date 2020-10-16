04d7ed8b4c6862a36d135d713f3ec9d8d684dedc
peka2 <d9xj@yahoo.co.jp>
2020-10-16 21:31:26 +0900

    Initial Commit

---
+++ TestWebView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestWebView
+//
+//  Created by Masayoshi Okada on 2020/10/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
