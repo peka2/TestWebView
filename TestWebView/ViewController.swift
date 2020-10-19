a95140011d03e9396cb92899f9bde0c9985c1263
peka2 <d9xj@yahoo.co.jp>
2020-10-19 17:42:58 +0900

    initial

--- TestWebView/ViewController.swift
+++ TestWebView/ViewController.swift
@@ -6,14 +6,31 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        
+        let url = URL(string: "https://cookpad.com/")!
+        
+        let request = URLRequest(url: url)
+        
+        webView.load(request)
+        
+        let newContentInset = UIEdgeInsets(top: 700, left: 0, bottom: 0, right: 0)
+        webView.scrollView.contentInset.top = newContentInset.top
 
+//        webView.setValue(newContentInset, forKey: "_obscuredInsets")
+//        webView.setValue(true, forKey: "_haveSetObscuredInsets")
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            self.webView.reload()
+        }
+        
+    }
 
 }
 
